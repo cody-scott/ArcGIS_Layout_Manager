380a9599081362d1968487e819ae9b139cdc7900
Cody Scott <jcodyscott@gmail.com>
2017-03-02 15:54:06 -0500

    Updated to 2.0

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='ArcGIS_Layout_Manager',
-    version='0.1',
+    version='0.2',
     description='Helper for supporting multiple layouts in ArcGIS layout view',
-    url='',
-    author='Cody Scott',
+    url='https://github.com/namur007/ArcGIS_Layout_Manager',
+    author='namur007',
     author_email='',
     license='MIT',
     packages=['ArcGIS_Layout_Manager'],
